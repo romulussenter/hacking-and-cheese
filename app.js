0ec316dad4721d4288255e084207804048d0707b
Romulus <romulus.senter@yahoo.com>
2018-01-30 20:45:24 -0500

    Added the random name genreator

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+//create your variables
 const button =document.querySelector('#generateBtn');
 const firstNameEl = document.querySelector('#first');
 const lastNameEl = document.querySelector('#last');
+const lastNames = [
+    'Gorgonzole' ,
+    'Shedder' ,
+    'Havarti' ,
+    'Head'
+];
+const firstNames = [
+    'Neo' ,
+    'Morpheus' ,
+    'Trinity' ,
+    'Poopy'
+];
 
+//add your eventlisteners
 button.addEventListener('click' ,generateName);
 
+//functions
 function generateName(e){
-    console.log(firstNameEl);
-    console.log(lastNameEl);
+    const rnLastName = randomChoice(lastNames);
+    const rnFirstName = randomChoice(firstNames);
+    firstNameEl.textContent =rnFirstName;
+    lastNameEl.textContent = rnLastName;
+}
+
+function randomChoice(array) {
+    const rnIndex = Math.floor (Math.random() * array.length);
+    return array[rnIndex];
 }
